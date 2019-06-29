6b64699a236d8353023eda2d7ee6d4d55ba408b5
manudevcode <manyoh.3y@gmail.com>
2019-06-29 15:55:58 -0500

    Solved: Item list tect overflowd

--- lib/activities_listing.dart
+++ lib/activities_listing.dart
@@ -68,52 +68,66 @@ class ActivtiesListingState extends State<ActivtiesListing> {
         body: new Container(
           child: ListView(
             children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: 60.0, left: 10.0, right: 15.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'most followed activities by 1,420 people in: ${this.title}',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        color: Color(getColorHexFromStr('#7A7A7A'))
-                      ),
-                    ),
-                    SizedBox(height: 20.0),
-                    Row(
+              Row(
+              children: <Widget>[
+                Flexible(
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                          child: Material(
-                            elevation: 0.0,
-                            borderRadius: BorderRadius.circular(25.0),
-                            child: Container(
-                              height: 100.0,
-                              width: 100.0,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10.0),
-                                image: new DecorationImage(image: AssetImage('assets/travel.png'), fit: BoxFit.cover),
+                        Row(children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                            child: Material(
+                              elevation: 0.0,
+                              borderRadius: BorderRadius.circular(25.0),
+                              child: Container(
+                                height: 110.0,
+                                width: 110.0,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  image: new DecorationImage(image: AssetImage('assets/travel.png'), fit: BoxFit.cover),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                            Text('Item list example', style: new TextStyle(fontSize: 18.0),),
-                            SizedBox(height: 20.0),
-                            Text('Description for item list example.', style: new TextStyle(fontSize: 14.0),),
-                            Text('Lorem ipsum in dolor consectetur.', style: new TextStyle(fontSize: 14.0),)
-                          ],),
-                        )
+                          Flexible(
+                            child: Padding(
+                              padding: EdgeInsets.only(top: 8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text("Item title", 
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24.0
+                                    ),
+                                  ),
+                                  SizedBox(height: 8.0,),
+                                  Text(
+                                    "Item subtitle",
+                                      style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.0,),
+                                  Text(
+                                    "Sunt dolor anim eu occaecat nulla officia minim do veniam irure veniam.",
+                                  )
+                                ],
+                              ),
+                            )
+                          )
+                        ],),
+                        
                       ],
-                    )
-                  ]
+                    ),
+                  ),
                 ),
-              ),   
+              ],
+            ),
             ],
           ),
         ),
